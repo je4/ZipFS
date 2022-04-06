f1d668a3ce452ce67ae2f1b4b90546a648a723e9
Jan-Erik Walker <janerik.walker@unibas.ch>
2022-04-06 17:37:09 +0200

    updated zipserver

--- cmd/zipserver.go
+++ cmd/zipserver.go
@@ -3,23 +3,20 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
 
-	_ "github.com/je4/ZipFS/v2/pgk/ZipFS"
+	"github.com/je4/ZipFS/v2/pkg/ZipFS"
 )
 
-func upload(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
-}
-
-func uploadFile(w http.ResponseWriter, r *http.Request) {
+func (sfs *ServerFS) uploadFile(w http.ResponseWriter, r *http.Request) {
 	// Maximum upload of 10 MB files
 	r.ParseMultipartForm(10 << 20)
 
 	// Get handler for filename, size and headers
-	file, handler, err := r.FormFile("myFile")
+	file, handler, err := r.FormFile("file")
 	if err != nil {
 		fmt.Println("Error Retrieving the File")
 		fmt.Println(err)
@@ -45,22 +42,38 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	zfs, err := NewZipFS(os.DirFS("../.."), dst)
+	zfs, err := ZipFS.NewZipFS(os.DirFS("."), dst.Name())
 	if err != nil {
 		log.Fatalf("cannot open test zip: %v", err)
 	}
-	defer zfs.Close()
+	// here is a problem? Error when not commented
+	// defer zfs.Close()
 
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
+	log.Println("zfs: ", zfs)
+
+	fname := "zip"
+	list, err := zfs.ReadDir(fname)
+	if err != nil {
+		log.Fatalf("cannot get directories from %s in zip: %v", fname, err)
+	}
+
+	sfs.fs = zfs
+	fmt.Fprintf(w, "Successfully Uploaded zip file second entry: %s\n", list[1].Name())
+}
+
+func displayZip(sfs ServerFS) http.Handler {
+	return http.FileServer(http.FS(sfs.fs))
+}
+
+type ServerFS struct {
+	fs fs.FS
 }
 
 func main() {
-	// mux := http.NewServeMux()
-	fsys := http.Dir("../testdata")
+	serverFS := ServerFS{fs: os.DirFS(".")}
 
-	fileServer := http.FileServer(fsys)
-	http.Handle("/static", fileServer)
-	http.HandleFunc("/upload", uploadFile)
+	http.Handle("/", displayZip(serverFS))
+	http.HandleFunc("/upload", serverFS.uploadFile)
 
 	err := http.ListenAndServe(":8080", nil)
 	log.Fatal(err)
